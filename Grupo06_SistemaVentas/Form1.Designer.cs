0f6f907f284a3b46e4007c05c302505d84724017
institucion-danielalcidescarrion <instituciondanielalcidescarrio@gmail.com>
2026-04-29 10:21:49 -0500

    1. Cambio 1-Joseph

--- Grupo06_SistemaVentas/Form1.Designer.cs
+++ Grupo06_SistemaVentas/Form1.Designer.cs
@@ -28,13 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(178, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(429, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FORMULARIO DE PRODUCTOS";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(590, 92);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 266);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(795, 183);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "LISTA DE PRODUCTOS";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(184, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "VER FORMULARIO MARCAS";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(378, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 60);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "VER FORMULARIO CATEGORIAS";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Grupo06_SistemaVentas/Form1.cs
+++ Grupo06_SistemaVentas/Form1.cs
@@ -15,6 +15,38 @@ namespace Grupo06_SistemaVentas
         public Form1()
         {
             InitializeComponent();
+            this.Load += Form1_Load;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+
+            dataGridView1.Columns.Add("Id", "ID");
+            dataGridView1.Columns.Add("Producto", "Producto");
+            dataGridView1.Columns.Add("Marca", "Marca");
+            dataGridView1.Columns.Add("Categoria", "Categoría");
+            dataGridView1.Columns.Add("Stock", "Stock");
+            dataGridView1.Columns.Add("Precio", "Precio");
+
+            dataGridView1.Rows.Add("1", "Laptop", "Lenovo", "Computadoras", "15", "2500.00");
+            dataGridView1.Rows.Add("2", "Mouse", "Logitech", "Accesorios", "40", "45.00");
+            dataGridView1.Rows.Add("3", "Teclado", "Redragon", "Accesorios", "25", "120.00");
+            dataGridView1.Rows.Add("4", "Monitor", "Samsung", "Pantallas", "10", "650.00");
+            dataGridView1.Rows.Add("5", "Impresora", "Epson", "Impresoras", "8", "780.00");
+            dataGridView1.Rows.Add("6", "Disco Duro", "Seagate", "Almacenamiento", "20", "320.00");
+            dataGridView1.Rows.Add("7", "Memoria RAM", "Kingston", "Componentes", "30", "180.00");
+            dataGridView1.Rows.Add("8", "Audífonos", "Sony", "Audio", "18", "210.00");
+            dataGridView1.Rows.Add("9", "Webcam", "Logitech", "Accesorios", "12", "150.00");
+            dataGridView1.Rows.Add("10", "Router", "TP-Link", "Redes", "14", "230.00");
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
         }
     }
+
 }
